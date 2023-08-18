381a6c5a086b79f0ee34430785a07b401571a8f7
sor4chi <sorakawa0218@gmail.com>
2023-08-18 18:30:52 +0900

    feat: implement server rpc

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,7 +1,13 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => c.text('Hello Hono!'))
+const router = app
+  .get("/", (c) => c.jsonT({ message: "Hello World" }))
+  .get("/hello/:name", (c) =>
+    c.jsonT({ message: `Hello ${c.req.param("name")}` })
+  );
 
-export default app
+export default app;
+
+export type AppType = typeof router;
