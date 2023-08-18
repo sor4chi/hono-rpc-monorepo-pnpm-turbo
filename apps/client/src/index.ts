99ef001d948da9cf656ff947c9c12ba25841abea
sor4chi <sorakawa0218@gmail.com>
2023-08-18 18:33:17 +0900

    feat: setting hono rpc

--- apps/client/src/index.ts
+++ apps/client/src/index.ts
@@ -1,6 +1,37 @@
 import { Hono } from 'hono'
+import { hc } from 'hono/client'
+import type { AppType } from 'server'
 
 const app = new Hono()
+
+const client = hc<AppType>('http://localhost:3000')
+/**
+const client: {
+    index: ClientRequest<{
+        $get: {
+            input: {};
+            output: {
+                message: string;
+            };
+        };
+    }>;
+} & {
+    hello: {
+        ":name": ClientRequest<{
+            $get: {
+                input: {
+                    param: {
+                        name: string;
+                    };
+                };
+                output: {
+                    message: string;
+                };
+            };
+        }>;
+    };
+}
+ */
 
 app.get('/', (c) => c.text('Hello Hono!'))
 
